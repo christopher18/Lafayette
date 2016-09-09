91e8ac9cb9cf69d9b6004bc045d73e4914f0106b
Zachary James Gospe <Zach@Zacharys-MacBook-Pro-2.local>
2016-09-09 16:04:00 -0500

    first commit

---
+++ Assets/MainCamera_Script.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCamera_Script : MonoBehaviour {
+
+	Transform target;
+	float distance;
+
+	// Use this for initialization
+	void Start () {
+		target = GameObject.Find("Player").transform;
+		distance = 2.5f;
+	}
+
+	void Update(){
+
+		GetComponent<Camera>().transform.position = new Vector3(
+			target.transform.position.x, 
+				target.transform.position.y,
+					target.transform.position.z - distance);
+
+	}
+}
---
+++ Assets/Player_Script.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Script : MonoBehaviour {
+
+	//base speed of the ship
+	float speed = 20;
+	float cannonPower = 2;
+	float totalProjectiles = 2;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+		
+	void FixedUpdate () {
+
+
+
+	}
+
+}
+}
